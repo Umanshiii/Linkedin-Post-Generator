0119e4b12dc85549ac04e32e48e3286f997587f0
Umanshiii <umanshi121@gmail.com>
2025-12-06 16:00:51 +0530

    Update settings.py

--- post_generator/settings.py
+++ post_generator/settings.py
@@ -8,6 +8,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 TEMPLATES = [
     {
@@ -77,9 +80,10 @@ DATABASES = {
     }
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = []
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
