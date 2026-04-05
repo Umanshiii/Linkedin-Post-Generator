485deee0b1f67357e33d8e499d63b9193f1d803e
Umanshiii <umanshi121@gmail.com>
2026-04-06 00:28:52 +0530

    Fixed static paths and collected files

--- post_generator/settings.py
+++ post_generator/settings.py
@@ -9,7 +9,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/./static'),] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend','build','static'),] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
